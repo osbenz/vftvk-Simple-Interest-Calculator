729687145df2068996538377fae69ad9a7c5619a
osbenz <65363715+osbenz@users.noreply.github.com>
2021-05-14 08:53:15 +0100

    edit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# coding-project-template
+Simple Interest Calculator.
+
+Context: Introduction to Cloud Development with HTML, CSS, and JavaScript Course, Offered by IBM on edX.
--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    interest = principal * rate * years / 100;
-    var year = new Date().getFullYear() + years;
+    var interest = principal * rate * years / 100;
+    var year = new Date().getFullYear() + parseInt(years);
     document.getElementById("result").innerHTML="If you deposit <mark>"
     +principal+"</mark>,<br>at an interest rate of <mark>"+rate+
-    "%</mark><br>You will receive an amount of <mark>"+amount+
-    "</mark>,<br>in the year "+year+"<br>";
+    "%</mark><br>You will receive an amount of <mark>"+interest+
+    "</mark>,<br>in the year <mark>"+year+"</mark>";
 }
 
 function updateRate() 
@@ -16,13 +16,3 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
 }
-
-(function () {
-    for(var i = 1; i <= 100; i++){
-        var select = document.getElementById("years");
-        var option = document.createElement("option");
-        option.text = i;
-        option.value = i;
-        select.options.add(option);
-    }
-})();
